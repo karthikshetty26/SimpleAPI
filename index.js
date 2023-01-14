8e9fb2efcc8cab78938acb637ea494a6f947e09f
Karthik Shetty <karthikkanyana26@gmail.com>
2023-01-14 18:32:55 +0530

    Updated ports

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import usersRouter from './routes/users.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
